0a70f5d2336f43f3d0b2393f68684b1faae41952
MacBookPro <matthewpalmer9@outlook.com>
2021-01-25 10:32:52 -0600

    Added the ability on the frontend to display messages for someone's list. To add and show items on a user's Christmas list.

--- src/components/MyList.jsx
+++ src/components/MyList.jsx
@@ -5,23 +5,41 @@ import { api } from '../services/api.js';
 export default function MyList(props) {
     const { authUser } = props;
     const [name, setName] = useState("");
+    const [list, setList] = useState([]);
+    const [listitems, setListitems] = useState([]);
     let history = useHistory();
 
     useEffect(() => {
         api.user.getUserList({user: authUser})
-        .then(resp => console.log(resp));
-        console.log("STATE:", name);
-    });
+        .then(resp => {
+            setList(resp.list)
+            setListitems(resp.listitems)
+            console.log(resp)
+        })
+        // .then(resp => setListitems(resp.listitems))
+    }, []);
 
     const handleChange = e => {
         setName(e.target.value)
+    }
+
+    const handleClick = () => {
+        console.log("Clicked")
     }
 
     return (
         <>
             {authUser.id ? (
                 <>
+                    <h1>{list.name}'s List</h1>
+                    {listitems.length === 0 ? (
+                        <>
+                            <h3>You have nothing in your list. Why not add something?</h3>
+                        </>
+                    ) : (<>Placeholder</>)}
+                    {/* {console.log(listitems)} */}
                     <input onChange={handleChange}></input>
+                    <button onClick={handleClick}> Add to List </button>
                     {name}
                 </>
             ) : (
